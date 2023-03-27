9835777f33b7e81f0dbb0225b478b9e309b0b759
jianghua <wujianghua@spgroup.com.sg>
2023-03-28 00:25:36 +0800

    Add export to build script

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -11,7 +12,11 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          {/* Welcome to <a href="https://nextjs.org">Next.js!</a> */}
+          Happy Birthday Skye!
+          <p>
+            Come on <Link href="/posts/first-post">in.</Link>
+          </p>
         </h1>
 
         <p className={styles.description}>
