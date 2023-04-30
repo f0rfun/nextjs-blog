97413697c4298039fb87436142198ec15b487895
skye <xianyikang@gmail.com>
2023-04-30 14:45:09 +0800

    test run 2

---
+++ .next/server/chunks/878.js
@@ -0,0 +1,258 @@
+exports.id = 878;
+exports.ids = [878];
+exports.modules = {
+
+/***/ 9544:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"container": "layout_container__fbLkO",
+	"header": "layout_header__kY0Lt",
+	"backToHome": "layout_backToHome__9sjx_"
+};
+
+
+/***/ }),
+
+/***/ 7530:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"heading2Xl": "utils_heading2Xl___9fFP",
+	"headingXl": "utils_headingXl__u25Y2",
+	"headingLg": "utils_headingLg__5535D",
+	"headingMd": "utils_headingMd__gD1Ok",
+	"borderCircle": "utils_borderCircle__s2nTm",
+	"colorInherit": "utils_colorInherit__mSH_x",
+	"padding1px": "utils_padding1px__PWQKR",
+	"list": "utils_list__4Mu4l",
+	"listItem": "utils_listItem__s2m6i",
+	"lightText": "utils_lightText__eUzGY"
+};
+
+
+/***/ }),
+
+/***/ 6772:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (/* binding */ Date)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+Object(function webpackMissingModule() { var e = new Error("Cannot find module 'date-fns'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+
+
+function Date({ dateString  }) {
+    const date = Object(function webpackMissingModule() { var e = new Error("Cannot find module 'date-fns'"); e.code = 'MODULE_NOT_FOUND'; throw e; }())(dateString);
+    return /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("time", {
+        dateTime: dateString,
+        children: Object(function webpackMissingModule() { var e = new Error("Cannot find module 'date-fns'"); e.code = 'MODULE_NOT_FOUND'; throw e; }())(date, "LLLL d, yyyy")
+    });
+}
+
+
+/***/ }),
+
+/***/ 8769:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "Z": () => (/* binding */ Layout),
+/* harmony export */   "y": () => (/* binding */ siteTitle)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(997);
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(968);
+/* harmony import */ var next_head__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(next_head__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var next_image__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(5675);
+/* harmony import */ var next_image__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(next_image__WEBPACK_IMPORTED_MODULE_2__);
+/* harmony import */ var _layout_module_css__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(9544);
+/* harmony import */ var _layout_module_css__WEBPACK_IMPORTED_MODULE_4___default = /*#__PURE__*/__webpack_require__.n(_layout_module_css__WEBPACK_IMPORTED_MODULE_4__);
+/* harmony import */ var _styles_utils_module_css__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(7530);
+/* harmony import */ var _styles_utils_module_css__WEBPACK_IMPORTED_MODULE_5___default = /*#__PURE__*/__webpack_require__.n(_styles_utils_module_css__WEBPACK_IMPORTED_MODULE_5__);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(1664);
+/* harmony import */ var next_link__WEBPACK_IMPORTED_MODULE_3___default = /*#__PURE__*/__webpack_require__.n(next_link__WEBPACK_IMPORTED_MODULE_3__);
+
+
+
+
+
+
+const name = "Skye Ng";
+const siteTitle = "Skye's Blog";
+function Layout({ children , home  }) {
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+        className: (_layout_module_css__WEBPACK_IMPORTED_MODULE_4___default().container),
+        children: [
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)((next_head__WEBPACK_IMPORTED_MODULE_1___default()), {
+                children: [
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("link", {
+                        rel: "icon",
+                        href: "data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>\uD83D\uDC66\uD83C\uDFFB</text></svg>"
+                    }),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("meta", {
+                        name: "description",
+                        content: "Skye Ng Yan Bin's personal website/blog"
+                    }),
+                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("meta", {
+                        name: "og:title",
+                        content: siteTitle
+                    })
+                ]
+            }),
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("header", {
+                className: (_layout_module_css__WEBPACK_IMPORTED_MODULE_4___default().header),
+                children: home ? /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {
+                    children: [
+                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_image__WEBPACK_IMPORTED_MODULE_2___default()), {
+                            priority: true,
+                            src: "/images/profile.png",
+                            className: (_styles_utils_module_css__WEBPACK_IMPORTED_MODULE_5___default().borderCircle),
+                            height: 144,
+                            width: 144,
+                            alt: ""
+                        }),
+                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("h1", {
+                            className: (_styles_utils_module_css__WEBPACK_IMPORTED_MODULE_5___default().heading2Xl),
+                            children: name
+                        })
+                    ]
+                }) : /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {
+                    children: [
+                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_3___default()), {
+                            href: "/",
+                            children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_image__WEBPACK_IMPORTED_MODULE_2___default()), {
+                                priority: true,
+                                src: "/images/profile.png",
+                                className: (_styles_utils_module_css__WEBPACK_IMPORTED_MODULE_5___default().borderCircle),
+                                height: 108,
+                                width: 108,
+                                alt: ""
+                            })
+                        }),
+                        /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("h2", {
+                            className: (_styles_utils_module_css__WEBPACK_IMPORTED_MODULE_5___default().headingLg),
+                            children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_3___default()), {
+                                href: "/",
+                                className: (_styles_utils_module_css__WEBPACK_IMPORTED_MODULE_5___default().colorInherit),
+                                children: name
+                            })
+                        })
+                    ]
+                })
+            }),
+            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("main", {
+                children: children
+            }),
+            !home && /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {
+                className: (_layout_module_css__WEBPACK_IMPORTED_MODULE_4___default().backToHome),
+                children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx((next_link__WEBPACK_IMPORTED_MODULE_3___default()), {
+                    href: "/",
+                    children: "← Back to home"
+                })
+            })
+        ]
+    });
+}
+
+
+/***/ }),
+
+/***/ 2479:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   "AU": () => (/* binding */ getPostData),
+/* harmony export */   "Le": () => (/* binding */ getAllPostIds),
+/* harmony export */   "ld": () => (/* binding */ getSortedPostsData)
+/* harmony export */ });
+/* harmony import */ var fs__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(7147);
+/* harmony import */ var fs__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(fs__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var path__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(1017);
+/* harmony import */ var path__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(path__WEBPACK_IMPORTED_MODULE_1__);
+Object(function webpackMissingModule() { var e = new Error("Cannot find module 'gray-matter'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+Object(function webpackMissingModule() { var e = new Error("Cannot find module 'remark'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+Object(function webpackMissingModule() { var e = new Error("Cannot find module 'remark-html'"); e.code = 'MODULE_NOT_FOUND'; throw e; }());
+
+
+
+
+
+const postsDirectory = path__WEBPACK_IMPORTED_MODULE_1___default().join(process.cwd(), "posts");
+function getSortedPostsData() {
+    // Get file names under /posts
+    const fileNames = fs__WEBPACK_IMPORTED_MODULE_0___default().readdirSync(postsDirectory);
+    const allPostsData = fileNames.map((fileName)=>{
+        // Remove ".md" from file name to get id
+        const id = fileName.replace(/\.md$/, "");
+        // Read markdown file as string
+        const fullPath = path__WEBPACK_IMPORTED_MODULE_1___default().join(postsDirectory, fileName);
+        const fileContents = fs__WEBPACK_IMPORTED_MODULE_0___default().readFileSync(fullPath, "utf8");
+        // Use gray-matter to parse the post metadata section
+        const matterResult = Object(function webpackMissingModule() { var e = new Error("Cannot find module 'gray-matter'"); e.code = 'MODULE_NOT_FOUND'; throw e; }())(fileContents);
+        // Combine the data with the id
+        return {
+            id,
+            ...matterResult.data
+        };
+    });
+    // Sort posts by date
+    return allPostsData.sort((a, b)=>{
+        if (a.date < b.date) {
+            return 1;
+        } else {
+            return -1;
+        }
+    });
+}
+function getAllPostIds() {
+    const fileNames = fs__WEBPACK_IMPORTED_MODULE_0___default().readdirSync(postsDirectory);
+    // Returns an array that looks like this:
+    // [
+    //   {
+    //     params: {
+    //       id: 'ssg-ssr'
+    //     }
+    //   },
+    //   {
+    //     params: {
+    //       id: 'pre-rendering'
+    //     }
+    //   }
+    // ]
+    return fileNames.map((fileName)=>{
+        return {
+            params: {
+                id: fileName.replace(/\.md$/, "")
+            }
+        };
+    });
+}
+async function getPostData(id) {
+    const fullPath = path__WEBPACK_IMPORTED_MODULE_1___default().join(postsDirectory, `${id}.md`);
+    const fileContents = fs__WEBPACK_IMPORTED_MODULE_0___default().readFileSync(fullPath, "utf8");
+    // Use gray-matter to parse the post metadata section
+    const matterResult = Object(function webpackMissingModule() { var e = new Error("Cannot find module 'gray-matter'"); e.code = 'MODULE_NOT_FOUND'; throw e; }())(fileContents);
+    // Use remark to convert markdown into HTML string
+    const processedContent = await Object(function webpackMissingModule() { var e = new Error("Cannot find module 'remark'"); e.code = 'MODULE_NOT_FOUND'; throw e; }())().use(Object(function webpackMissingModule() { var e = new Error("Cannot find module 'remark-html'"); e.code = 'MODULE_NOT_FOUND'; throw e; }())).process(matterResult.content);
+    const contentHtml = processedContent.toString();
+    // Combine the data with the id
+    return {
+        id,
+        contentHtml,
+        ...matterResult.data
+    };
+}
+
+
+/***/ })
+
+};
+;
